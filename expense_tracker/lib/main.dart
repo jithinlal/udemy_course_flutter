d9ed1076ad20cf81ac35b527da4046f928846405
jithinlal <jithtitan007@gmail.com>
2019-09-15 17:09:57 +0530

    expense tracker  - transaction add

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -38,6 +38,7 @@ class MyHomePage extends StatelessWidget {
         children: <Widget>[
           Card(
             color: Colors.blue,
+            elevation: 5,
             child: Container(
               width: double.infinity,
               child: Text(
@@ -45,11 +46,57 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text(
-              'List',
-            ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(
+                        10,
+                      ),
+                      child: Text(
+                        '\$ ${tx.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
